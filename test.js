0bca7c06ca81a148eb4645b6e620ed8a8dbbdef6
kah3vich <nik.b20@mail.ru>
2023-02-26 14:28:20 +0300

    test: version

--- test.js
+++ test.js
@@ -9,28 +9,30 @@ const arrTriggerWordOther = [
 	'let ',
 	'var ',
 	'function ',
+	'switch ',
 	'(',
+	' () ',
 	'enum ',
 	'interface ',
+	'new ',
 	'class ',
 	'return ',
 	'true',
 	'false',
 	' = ',
+	' => ',
 	' == ',
 	' != ',
 	' === ',
 	' !== ',
-	...[
-		...'abcdefghijklmnopqrstuvwxyz'
-			.toLocaleLowerCase()
-			.split('')
-			.map(el => `type ${el}`),
-		...'abcdefghijklmnopqrstuvwxyz'
-			.toLocaleUpperCase()
-			.split('')
-			.map(el => `type ${el}`),
-	],
+	...'abcdefghijklmnopqrstuvwxyz'
+		.toLocaleLowerCase()
+		.split('')
+		.map(el => `type ${el}`),
+	...'abcdefghijklmnopqrstuvwxyz'
+		.toLocaleUpperCase()
+		.split('')
+		.map(el => `type ${el}`),
 ];
 
 const getPartCode = (code, type) => {
@@ -64,6 +66,7 @@ const getPartCode = (code, type) => {
 const code = `
 import { toggleGift } from '../app';
 import 'app'
+import React, { React as NotReact, Old } from 'react'
 import {
   getLocalStorage,
   getQueryParameter,
@@ -84,10 +87,10 @@ import {
 } from './functions';
 import { USER, CART, MENU, DADATA } from './variable';
 import notification from './notification';
-import { route } from '../app/route';
-import { Router } from '../app/route';
+import type { route } from '../app/route';
+import  { Router } from '../app/route';
 import '../js/plugins/device';
-
+import * from './hello'
 import {
   STORAGE_CART,
   STORAGE_COUPONS,
@@ -108,21 +111,42 @@ let scrollListener = false;
 
 `;
 
+const configExtension = [
+	{
+		importDefault: ['React'],
+		importExport: ['useState'],
+		importOnly: false,
+		importType: [],
+		importAll: false,
+		importAsAll: '',
+		package: 'react',
+	},
+	{
+		importDefault: [],
+		importExport: ['createStory'],
+		importOnly: false,
+		importType: [],
+		importAll: false,
+		importAsAll: '',
+		package: 'react',
+	},
+];
+
 const removeDuplicates = (arr, key) => {
-	let lookup = {};
-	let result = [];
-	for (let i = 0; i < arr.length; i++) {
-		let item = arr[i];
-		if (!lookup[item[key]]) {
-			result.push(item);
-			lookup[item[key]] = true;
-		}
-	}
-	return result;
+	const seen = {};
+	return arr.filter(item => {
+		const k = item[key];
+		return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+	});
 };
 
+const arrayOfLetters = [
+	...'abcdefghijklmnopqrstuvwxyz'.toLocaleLowerCase().split(''),
+	...'abcdefghijklmnopqrstuvwxyz'.toLocaleUpperCase().split(''),
+	...'*$'.split(''),
+];
+
 const convertImportInStringToObjectImports = arrImport => {
-	const result = [];
 	const predResult = [];
 
 	arrImport.forEach(elemImport => {
@@ -131,39 +155,199 @@ const convertImportInStringToObjectImports = arrImport => {
 			importExport: [],
 			importOnly: false,
 			importType: [],
-			importAs: [],
+			importAll: false,
 			importAsAll: '',
 			package: elemImport.replace('"', "'").match(/'(.*?)'/)[1],
 		});
 	});
 
+	const result = copyArray(removeDuplicates(predResult, 'package'));
+
 	arrImport.forEach(elemImport => {
-		removeDuplicates(predResult, 'package').forEach(elemObject => {
+		result.forEach(elemObject => {
 			if (elemImport.replace('"', "'").match(/'(.*?)'/)[1] === elemObject.package) {
-				if (elemImport.includes('import') && !elemImport.includes('from')) {
-					result.push({
-						importDefault: elemObject.importDefault || [],
-						importExport: elemObject.importExport || [],
-						importOnly: elemObject.importOnly || false,
-						importType: elemObject.importType || [],
-						importAs: elemObject.importAs || [],
-						importAsAll: elemObject.importAsAll || '',
-						package: elemObject.package,
-					});
+				if (elemImport.includes('import * from ')) {
+					elemObject.importAll = true;
 				}
 
 				if (elemImport.includes('import * as ') && elemImport.includes(' from ')) {
-					result.push({
-						importDefault: elemObject.importDefault || [],
-						importExport: elemObject.importExport || [],
-						importOnly: elemObject.importOnly || false,
-						importType: elemObject.importType || [],
-						importAs: elemObject.importAs || [],
-						importAsAll: elemImport
-							.replace('import * as ', '')
-							.slice(0, elemImport.indexOf(' from ')),
-						package: elemObject.package,
-					});
+					elemObject.importAsAll = elemImport
+						.replace('import ', '')
+						.slice(0, elemImport.indexOf(' from ') - 7);
+				}
+
+				if (
+					elemImport.includes('import type ') &&
+					elemImport.includes(' from ') &&
+					!elemImport.includes('import * ') &&
+					!elemImport.includes('import * as ')
+				) {
+					let exportsArr = [];
+					let defaultsArr = [];
+					let wordTrigger = [];
+
+					let checkWord = true;
+					let checkDefault = true;
+					let activeWordAs = false;
+
+					elemImport
+						.replace('import type ', '')
+						.slice(0, elemImport.indexOf(' from ') - 7)
+						.replace(" from '", '')
+						.split('')
+						.forEach(el => {
+							if (el === '{') {
+								checkDefault = false;
+								activeWordAs = true;
+							}
+							if (el === '}') {
+								if (wordTrigger.length) {
+									if (checkDefault) {
+										defaultsArr.push(wordTrigger.join('').trim());
+									} else {
+										exportsArr.push(wordTrigger.join('').trim());
+									}
+								}
+								wordTrigger = [];
+								checkWord = false;
+								checkDefault = true;
+							}
+
+							if (wordTrigger.join('') == 'from') {
+								wordTrigger = [];
+								checkWord = false;
+							}
+
+							if (el === ' ') {
+								if (activeWordAs) {
+									wordTrigger.push(el);
+									checkWord = true;
+								} else {
+									if (wordTrigger.length) {
+										if (checkDefault) {
+											defaultsArr.push(wordTrigger.join('').trim());
+										} else {
+											exportsArr.push(wordTrigger.join('').trim());
+										}
+									}
+									wordTrigger = [];
+									checkWord = false;
+								}
+							}
+							if (el === ',') {
+								if (wordTrigger.length) {
+									if (checkDefault) {
+										defaultsArr.push(wordTrigger.join('').trim());
+									} else {
+										exportsArr.push(wordTrigger.join('').trim());
+									}
+								}
+								wordTrigger = [];
+								checkWord = false;
+							}
+							arrayOfLetters.forEach(word => {
+								if (el === word) {
+									wordTrigger.push(el);
+
+									checkWord = true;
+								}
+							});
+						});
+					elemObject.importType = exportsArr;
+				}
+
+				if (
+					elemImport.includes('import ') &&
+					elemImport.includes(' from ') &&
+					!elemImport.includes('import type ') &&
+					!elemImport.includes('import * as ')
+				) {
+					let exportsArr = [];
+					let defaultsArr = [];
+					let wordTrigger = [];
+
+					let checkWord = true;
+					let checkDefault = true;
+					let activeWordAs = false;
+
+					elemImport
+						.replace('import ', '')
+						.slice(0, elemImport.indexOf(' from ') - 7)
+						.replace(" from '", '')
+						.split('')
+						.forEach(el => {
+							if (el === '{') {
+								checkDefault = false;
+								activeWordAs = true;
+							}
+							if (el === '}') {
+								if (wordTrigger.length) {
+									if (checkDefault) {
+										defaultsArr.push(wordTrigger.join('').trim());
+									} else {
+										exportsArr.push(wordTrigger.join('').trim());
+									}
+								}
+								wordTrigger = [];
+								checkWord = false;
+								checkDefault = true;
+							}
+
+							if (wordTrigger.join('') == 'type') {
+								wordTrigger = [];
+								checkWord = false;
+							}
+
+							if (wordTrigger.join('') == 'from') {
+								wordTrigger = [];
+								checkWord = false;
+							}
+
+							if (el === ' ') {
+								if (activeWordAs) {
+									wordTrigger.push(el);
+									checkWord = true;
+								} else {
+									if (wordTrigger.length) {
+										if (checkDefault) {
+											defaultsArr.push(wordTrigger.join('').trim());
+										} else {
+											exportsArr.push(wordTrigger.join('').trim());
+										}
+									}
+									wordTrigger = [];
+									checkWord = false;
+								}
+							}
+							if (el === ',') {
+								if (wordTrigger.length) {
+									if (checkDefault) {
+										defaultsArr.push(wordTrigger.join('').trim());
+									} else {
+										exportsArr.push(wordTrigger.join('').trim());
+									}
+								}
+								wordTrigger = [];
+								checkWord = false;
+							}
+							arrayOfLetters.forEach(word => {
+								if (el === word) {
+									wordTrigger.push(el);
+
+									checkWord = true;
+								}
+							});
+						});
+					elemObject.importDefault = defaultsArr;
+					elemObject.importExport = exportsArr;
+				}
+
+				if (
+					elemImport.includes('import ') &&
+					!elemImport.includes(' from ') &&
+					!elemImport.includes('import type ')
+				) {
+					elemObject.importOnly = true;
 				}
 			}
 		});
@@ -182,5 +366,9 @@ const codeImportsFile = getPartCode(code, 'import')
 const codeMainFile = getPartCode(code, 'main');
 
 const arrImportsObject = convertImportInStringToObjectImports(codeImportsFile);
+
+const connectImportsFileWithConfigImports = (arrImports, arrConfig) => {};
+
+const allArrayImports = connectImportsFileWithConfigImports(arrImportsObject, configExtension);
 
 console.log('✅ arrImportsObject    ', arrImportsObject);
